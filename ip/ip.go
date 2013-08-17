dc97899555fe961ac9923d58c457db70f72f0b62
Dmitry Alenichev <mitya@rockers.su>
2013-08-18 03:43:38 +0400

    skip some files, template output, bump version

--- ip/ip.go
+++ ip/ip.go
@@ -1,7 +1,7 @@
 package ip
 
 import (
-    "fmt"
+    "html/template"
     "net/http"
 )
 
@@ -10,5 +10,44 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, r.RemoteAddr)
+    err := indexTemplate.Execute(w, r.RemoteAddr)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
+
+var indexTemplate = template.Must(template.New("handler").Parse(indexTemplateHTML))
+
+const indexTemplateHTML = `
+<html>
+  <body>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter22091863 = new Ya.Metrika({id:22091863,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/22091863" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+    <pre>{{.}}</pre>
+  </body>
+</html>
+`
